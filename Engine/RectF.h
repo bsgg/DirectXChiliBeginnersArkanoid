c76c62343459705ed364f14cec49143aa7053ba2
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2017-02-03 08:51:24 +0000

    Added RectF, Vect2

---
+++ Engine/RectF.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Vec2.h"
+
+class RectF
+{
+
+public:
+	// Default constructor, including this function, the compiler won't deactivate the default compilare when there is 
+	// another compiler with params.
+	RectF() = default;	
+	RectF(float left_in, float right_in, float top_in, float bottom_in);
+	RectF(const Vec2& topLeft, const Vec2& bottomRight);
+	RectF(const Vec2& topLeft, float width, float height);
+
+	bool IsOverlappingWidth(const RectF& other) const;
+
+public:
+	float left;
+	float right;
+	float top;
+	float bottom;	 
+};
